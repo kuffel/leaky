6aa9dc56b7584d2dd3ad7af0cd835e21c322240b
Adam Kuffel <adam.kuffel@lemonbeat.com>
2021-07-15 15:45:47 +0200

    Added index

--- lib/leaky.ex
+++ lib/leaky.ex
@@ -5,27 +5,27 @@ defmodule Leaky do
   require Logger
 
   def demo() do
-    Enum.each(1..1_000, fn _i ->
-      run_os_cmd("sleep 2")
+    Enum.each(1..1_000, fn i ->
+      run_os_cmd("sleep 2", i)
     end)
   end
 
-  def run_os_cmd(cmd) do
+  def run_os_cmd(cmd, i) do
     spawn(fn ->
       :exec.run(cmd, [:stdin, :stdout, :stderr, :monitor])
 
       receive do
         {:stdout, _os_pid, result} ->
-          Logger.info("#{result}")
+          Logger.info("#{i}: #{result}")
 
         {:DOWN, _os_pid, :process, _port, reason} ->
-          Logger.info("Finished #{inspect(cmd)} with reason: #{inspect(reason)}")
+          Logger.info("#{i}: Finished #{inspect(cmd)} with reason: #{inspect(reason)}")
 
         other ->
-          Logger.warn("Unmatched reply: #{inspect(other)}")
+          Logger.warn("#{i}: Unmatched reply: #{inspect(other)}")
       after
         60_000 ->
-          Logger.error("Timeout calling: #{inspect(cmd)}")
+          Logger.error("#{i}: Timeout calling: #{inspect(cmd)}")
       end
     end)
   end
